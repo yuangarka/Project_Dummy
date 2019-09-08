86a9b255be96abfed59ce9cf36a21e2f0f7ed34e
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2019-09-08 14:13:25 +0700

    PR pindah activity

--- app/src/main/java/com/katouji/project_dummy/HomeActivity.java
+++ app/src/main/java/com/katouji/project_dummy/HomeActivity.java
@@ -2,6 +2,7 @@ package com.katouji.project_dummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,8 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void openActivityAbout(){
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/katouji/project_dummy/MainActivity.java
+++ app/src/main/java/com/katouji/project_dummy/MainActivity.java
@@ -2,13 +2,48 @@ package com.katouji.project_dummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtn;
+    private EditText emailTxt;
+    private EditText passTxt;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailTxt = findViewById(R.id.emailTxt);
+        passTxt = findViewById(R.id.passTxt);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (emailTxt.getText().toString().trim().matches(emailPattern)){
+                    if (passTxt.getText().toString().isEmpty()){
+                        Toast.makeText(getApplicationContext(),"Password is empty !",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        openArctivityHome();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Input email and password !",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void openArctivityHome(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
