d9d7633617d12ceef1b7f3bd3ef975b95f8beed2
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2019-09-15 20:19:27 +0700

    Refisi Fragment dan PR

--- app/src/main/java/com/katouji/project_dummy/HomeActivity.java
+++ app/src/main/java/com/katouji/project_dummy/HomeActivity.java
@@ -1,21 +1,22 @@
 package com.katouji.project_dummy;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends FragmentActivity {
+public class HomeActivity extends AppCompatActivity {
     private Button btnAbout;
-    private Button btnFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_one);
+        setContentView(R.layout.activity_home);
 
         btnAbout = (Button) findViewById(R.id.btnAbout);
         btnAbout.setOnClickListener(new View.OnClickListener() {
@@ -32,4 +33,37 @@ public class HomeActivity extends FragmentActivity {
         startActivity(intent);
 
     }
+
+    public void loadFragmentOne(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment1 = fragmentManager.findFragmentByTag("fragOne");
+
+        if (fragment1 == null) {
+            fragment1 = new FragmentOne();
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainFrag, fragment1, "fragOne");
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void loadFragmentTwo(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment2 = fragmentManager.findFragmentByTag("fragTwo");
+
+        if (fragment2 == null) {
+            fragment2 = new FragmentTwo();
+        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.mainFrag, fragment2, "fragTwo");
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
