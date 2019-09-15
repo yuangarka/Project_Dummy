e0d415ace55b5de36a8ff0df8e0854d583440060
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2019-09-15 20:29:57 +0700

    Refisi Fragment dan PR

---
+++ app/src/main/java/com/katouji/project_dummy/FragmentOne.java
@@ -0,0 +1,27 @@
+package com.katouji.project_dummy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class FragmentOne extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one,container,false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
