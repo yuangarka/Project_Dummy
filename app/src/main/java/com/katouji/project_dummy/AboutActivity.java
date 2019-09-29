79c10338a64256fdb9489c442a75022595e731d3
yuangarka <apholos.yuang@ti.ukdw.ac.id>
2019-09-30 00:12:42 +0700

    Tambah Recycle View di activity about

--- app/src/main/java/com/katouji/project_dummy/AboutActivity.java
+++ app/src/main/java/com/katouji/project_dummy/AboutActivity.java
@@ -1,14 +1,43 @@
 package com.katouji.project_dummy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
 public class AboutActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        listData = new ArrayList<>();
+        setData();
+
+        recyclerView = findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new RecycleAdapter(listData);
+        recyclerView.setAdapter(adapter);
     }
+
+    private void setData(){
+        listData.add("Data Satu");
+        listData.add("Data Dua");
+        listData.add("Data Tiga");
+        listData.add("Data Empat");
+        listData.add("Data Lima");
+    }
+
 }
